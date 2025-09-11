fb8af78774d82ff22c0b43887522d1d706ade357
Yoné Toussaint <147109802+yonetoussaint@users.noreply.github.com>
2025-09-11 15:10:19 -0400

    Create ProductHeaderSection.tsx

---
+++ src/components/product/ProductHeaderSection.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import ProductHeader from "@/components/product/ProductHeader";
+
+interface ProductHeaderSectionProps {
+  ref: React.RefObject<HTMLDivElement>;
+  activeSection: string;
+  onTabChange: (section: string) => void;
+  focusMode: boolean;
+  showHeaderInFocus: boolean;
+  onProductDetailsClick: () => void;
+  currentImageIndex: number;
+  totalImages: number;
+  onShareClick: () => void;
+}
+
+const ProductHeaderSection: React.FC<ProductHeaderSectionProps> = ({
+  ref,
+  activeSection,
+  onTabChange,
+  focusMode,
+  showHeaderInFocus,
+  onProductDetailsClick,
+  currentImageIndex,
+  totalImages,
+  onShareClick
+}) => {
+  return (
+    <div ref={ref} className="relative z-50">
+      <ProductHeader 
+        activeSection={activeSection}
+        onTabChange={onTabChange}
+        focusMode={focusMode}
+        showHeaderInFocus={showHeaderInFocus}
+        onProductDetailsClick={onProductDetailsClick}
+        currentImageIndex={currentImageIndex}
+        totalImages={totalImages}
+        onShareClick={onShareClick}
+      />
+    </div>
+  );
+};
+
+export default ProductHeaderSection;
